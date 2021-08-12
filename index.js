d1981e4b9ccef25c71ebbd601ab3493d831151cd
skdksldk <tnwns1485@naver.com>
2021-08-12 19:47:23 +0900

    로그인 Route 만드는중

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+
 const { User } = require("./models/User")
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(bodyParser.json())
 
@@ -17,16 +20,43 @@ mongoose.connect('mongodb+srv://tnwns1485:1234@cluster0.smu2n.mongodb.net/myFirs
     }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
-app.post('/register', (req,res) => {
-    
+app.post('/register', (req, res) => {
+
 
     const user = new User(req.body)
 
     user.save((err, userInfo) => {
-        if(err) return res.json({ success: false, err})
-            return res.status(200).json({
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
             success: true
         })
+    })
+
+})
+
+app.post('/login', (req, res) => {
+
+
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+                return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+
+
+            user.generateToken((err, user) => {
+
+            })
+
+        })
+
+
     })
 
 })
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +32,35 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function(next) {
+    var user = this;
+    if (user.isModified('password')) {
+
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err),
+            cb(null, isMatch)
+    })
+}
+
+
+
 
 const User = mongoose.model('User', userSchema)
 
