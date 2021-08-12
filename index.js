df3f20593f4e7e14c14dedc4c9ab9a9b8317527e
skdksldk <tnwns1485@naver.com>
2021-08-12 17:59:24 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require("./models/User")
 
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://tnwns1485:1234@cluster0.smu2n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
@@ -11,6 +16,20 @@ mongoose.connect('mongodb+srv://tnwns1485:1234@cluster0.smu2n.mongodb.net/myFirs
         useFindAndModify: false
     }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
+
+app.post('/register', (req,res) => {
+    
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+            return res.status(200).json({
+            success: true
+        })
+    })
+
+})
 
 
 app.get('/', (req, res) => res.send('Hello World!~~안녕하세요 ~ '))
